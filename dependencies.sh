27972a7fa487342c0ba81db4959bcd38c0b50f9a
vrajadh <vrajadh@ncsu.edu>
2016-09-13 08:44:23 -0400

    changed the dependencies file

--- dependencies.sh
+++ dependencies.sh
@@ -7,15 +7,3 @@ npm install
 cd ../aws
 npm install
 
-cd ..
-
-sudo apt-get update
-sudo apt-get install nodejs
-sudo apt-get install make vim python-dev python-pip
-git clone git://github.com/ansible/ansible.git --recursive
-cd ./ansible
-git checkout tags/v1.9.2-1
-git submodule update --recursive
-source ./hacking/env-setup
-
-
